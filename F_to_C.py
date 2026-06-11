99e95bebbc9aa92b4e44a2a11ced06c7fbe1c606
Trevor Youmans <corruptedconsciousness@proton.me>
2026-06-11 12:21:06 -0400

    Fix typo in function name and improve comments

--- F_to_C.py
+++ F_to_C.py
@@ -1,9 +1,11 @@
 # Converts degress Celsius to degrees Fahrenheit.
+
 def farenheit_celcius(f_temp):
     c_temp = (f_temp - 32) * (5 / 9)
     return c_temp
 
 # Asks user for Celsius input, performs conversion,
 # and returns the result.
+
 result = farenheit_celcius(98.6)
 print(f"98.6 F converts to {result} C.")
